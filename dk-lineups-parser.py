fea309f9c9925e6d732df30b0fce2f3c38d8c999
github username <github email address>
2020-11-17 22:31:50 -0600

    added feature to find player ownership % by username

--- dk-lineups-parser.py
+++ dk-lineups-parser.py
@@ -1,34 +1,63 @@
 import csv
 
-def find_contest_player_ownership(file):
+payouts = [
+    [1, 200000],
+    [2, 50000],
+    [3, 25000],
+    [4, 10000],
+    [5, 7500],
+    [6, 5000],
+    [8, 2500],
+    [10, 2000],
+    [13, 1500],
+    [16, 1000],
+    [20, 750],
+    [25, 600],
+    [35, 500],
+    [45, 400],
+    [60, 300],
+    [75, 250],
+    [99, 200],
+    [120, 150],
+    [195, 100],
+    [315, 75],
+    [565, 60],
+    [1165, 50],
+    [2356, 40],
+    [7215, 30]
+]
 
+def find_contest_player_ownership(file, username = None):
     with open(file, 'r') as csv_file:
         csv_reader = csv.reader(csv_file)
         # this skips the first line of the file
         next(csv_reader)
 
         # returns a list and int
-        players_list, total_rosters = create_list_of_players(csv_reader)
+        players_list, total_rosters = create_list_of_players(csv_reader, username)
         # returns an object
         player_frequency = get_player_frequency(players_list)
         find_ownership_percentages(player_frequency, total_rosters)
 
-
-def create_list_of_players(csv_file):
+# optional argument to find player ownership for specific users.
+def create_list_of_players(csv_file, username = None):
     players = []
     total_rosters = 0
     for line in csv_file:
-            total_rosters += 1
-            roster = line[5]
-            split = roster.split()
-            player_name = ""
-            positions = ["QB", "RB", "WR", "TE", "FLEX", "DST"]
-            for x in range(len(split)):
-                if split[x] in positions:
-                    players.append(player_name.rstrip())
-                    player_name = ""
-                else:
-                    player_name += f'{split[x]} '
+        if username:
+            if line[2].split(' ')[0] != username:
+                continue
+        total_rosters += 1
+        roster = line[5]
+        split = roster.split()
+        player_name = ""
+        positions = ["QB", "RB", "WR", "TE", "FLEX", "DST"]
+        for x in range(len(split)):
+            if split[x] in positions:
+                players.append(player_name.rstrip())
+                player_name = ""
+            else:
+                player_name += f'{split[x]} '
     return players, total_rosters
 
 def get_player_frequency(players_list):
@@ -45,10 +74,11 @@ def get_player_frequency(players_list):
 def find_ownership_percentages(player_frequency, total_rosters):
     for player in player_frequency:
             if round(player_frequency[player] / total_rosters, 2) > 0.01:
-                player_frequency[player] = round(player_frequency[player] / total_rosters, 2)
+                player_frequency[player] = round(player_frequency[player] / total_rosters *100)
                 print(f'{player}: {player_frequency[player]}%')
 
 
-find_contest_player_ownership('600K-wk10-2020.csv')
 
+
+#find_contest_player_ownership('600K-wk10-2020.csv', "username")
 
